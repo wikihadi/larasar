c9855e2fbc36866ba3cd1f8bd4d1b328c51158aa
rafeee <rafeee.ir@gmail.com>
2020-01-20 12:42:49 +0330

    Larasar

---
+++ ui/src/pages/Index.vue
@@ -0,0 +1,14 @@
+<template>
+  <q-page class="flex flex-center">
+    <div>
+    <h1>Larasa</h1>
+    <h4>Laravel + Quasar</h4>
+    </div>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex'
+}
+</script>
